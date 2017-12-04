f5b1da88bd1f1a0045582d5f950f661b0c41c2eb
Banana Android Z <ndrdbnnz@gmail.com>
2017-12-04 18:59:11 +0300

    new

--- src/Bot.java
+++ src/Bot.java
@@ -18,30 +18,17 @@ public class Bot extends TelegramLongPollingBot {
                e.printStackTrace();
            }
        }
-
+       Update update;
+    Message message = update.getMessage();
     @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
+
+        message.getText().toLowerCase();
         if (message != null && message.hasText()) {
             if (message.getText().equals("/help")) {
-                sendMsg(message,"Проверка на /help выполнена");
-            } else if (message.getText().equals("привет") || message.getText().equals("hello")) {
-                sendMsg(message,"Привет пользователь!");
-            } else if (message.getText().equals("что умеешь делать")) {
-                sendMsg(message,"Приветствовать и прощаться");
-            } else if (message.getText().equals("пока")) {
-                sendMsg(message, "Пока пользователь!");
-            } else if (message.getText().equals("как дела")) {
-                sendMsg(message, "Все прекрасно!   Как у вас?");
-                if (message.getText().equals("хорошо") || message.getText().equals("нормально") || message.getText().equals("отлично")) {
-                    sendMsg(message, "Я рад за вас!");
-                } else {
-                    sendMsg(message, "Я могу помочь?");
-                    if (message.getText().equals("да")) {
-                        sendMsg(message, "Чем?");
-                        help();
-                    }
-                }
+                instruction();
+            }  else {
+                sendMsg(message,"непонятно");
             }
         }
     }
@@ -56,6 +43,10 @@ public class Bot extends TelegramLongPollingBot {
            } catch (TelegramApiException e) {
                e.printStackTrace();
            }
+    }
+    private void instruction() {
+           sendMsg(message, "это инструкция");
+
     }
     private void help() {
         SendMessage sendMessage = new SendMessage();
